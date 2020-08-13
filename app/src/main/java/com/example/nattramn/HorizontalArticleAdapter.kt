7d0a2fa5bd09741e3124ff2e39274cb926527cd3
HosseinMirzaei47 <hosseinhidd@gmail.com>
2020-08-13 17:39:29 +0430

    Created activity_home.xml. Implemented toolbar, search field and the vertical RecyclerView.

---
+++ app/src/main/java/com/example/nattramn/HorizontalArticleAdapter.kt
@@ -0,0 +1,42 @@
+package com.example.nattramn
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import de.hdodenhof.circleimageview.CircleImageView
+import kotlinx.android.synthetic.main.horizontal_article_row.view.*
+
+class HorizontalArticleAdapter(val context: Context) :
+    RecyclerView.Adapter<HorizontalArticleAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.horizontal_article_row, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        if (position % 3 == 0) {
+            holder.bookmarkItem.setImageResource(R.drawable.ic_bookmark)
+            holder.authorImage.setImageResource(R.drawable.test01)
+            holder.articlePreview.text = context.resources.getString(R.string.news)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return 15
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val authorImage: CircleImageView = itemView.itemAuthorImage
+        val authorName: TextView = itemView.itemAuthorName
+        val articleTime: TextView = itemView.itemTime
+        val bookmarkItem: ImageButton = itemView.itemBookmark
+        val articlePreview: TextView = itemView.itemArticlePreview
+    }
+
+}
--- app/src/main/java/com/example/nattramn/MainActivity.kt
+++ app/src/main/java/com/example/nattramn/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.nattramn
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_home.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_home)
+
+        val adapter = HorizontalArticleAdapter(this)
+        recyclerHomeArticle.layoutManager = LinearLayoutManager(this)
+        recyclerHomeArticle.adapter = adapter
+
     }
 }
