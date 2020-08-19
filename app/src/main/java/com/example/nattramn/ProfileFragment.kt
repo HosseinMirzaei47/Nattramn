e95b6e4f6fb0f02625a5dd0bc09d3c195dadaa2e
HosseinMirzaei47 <hosseinhidd@gmail.com>
2020-08-19 23:12:43 +0430

    fixed issue #13

--- app/src/main/java/com/example/nattramn/ProfileFragment.kt
+++ app/src/main/java/com/example/nattramn/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.example.nattramn
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.Gravity
 import android.view.LayoutInflater
@@ -9,6 +10,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.nattramn.adapters.ProfileArticleAdapter
 import com.github.rubensousa.gravitysnaphelper.GravitySnapHelper
+import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment : Fragment() {
@@ -21,13 +23,45 @@ class ProfileFragment : Fragment() {
         return LayoutInflater.from(context).inflate(R.layout.fragment_profile, container, false)
     }
 
+    @SuppressLint("InflateParams")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setTabItemsView()
 
         setBackButtonClick()
 
         setRecyclers()
 
+    }
+
+    private fun setTabItemsView() {
+        profileTab.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+
+            @SuppressLint("InflateParams")
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+
+                profileTab.getTabAt(0)?.customView = null
+                profileTab.getTabAt(1)?.customView = null
+
+                val tabSelectedView: View? =
+                    LayoutInflater.from(context).inflate(R.layout.custom_tab_selected, null)
+
+                if (tab?.position == 0 && profileTab.getTabAt(0)?.customView != tabSelectedView) {
+                    profileTab.getTabAt(0)?.customView = tabSelectedView
+                } else {
+                    profileTab.getTabAt(1)?.customView = tabSelectedView
+                }
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+        })
     }
 
     private fun setRecyclers() {
