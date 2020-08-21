26ed0720f3182dca5015ce8c92814828f8e3510a
HosseinMirzaei47 <hosseinhidd@gmail.com>
2020-08-21 13:55:45 +0430

    Implemented isValidUsername in LoginFragment.kt.

--- app/src/main/java/com/example/nattramn/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/nattramn/fragments/LoginFragment.kt
@@ -1,6 +1,7 @@
 package com.example.nattramn.fragments
 
 import android.os.Bundle
+import android.util.Patterns
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +11,10 @@ import com.example.nattramn.R
 import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginFragment : Fragment() {
+
+    companion object {
+        private const val minUsernameLength = 7
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,7 +26,6 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         showSystemUI()
 
         buttonOnClicks()
@@ -31,8 +35,12 @@ class LoginFragment : Fragment() {
     private fun buttonOnClicks() {
 
         loginEnterButton.setOnClickListener { view ->
-            Navigation.findNavController(view)
-                .navigate(LoginFragmentDirections.actionLoginFragmentToHomeFragment())
+
+            if (loginUsername.text!!.isValidUsername()) {
+                Navigation.findNavController(view)
+                    .navigate(LoginFragmentDirections.actionLoginFragmentToHomeFragment())
+            }
+
         }
 
         loginRegisterButton.setOnClickListener { view ->
@@ -40,6 +48,33 @@ class LoginFragment : Fragment() {
                 .navigate(LoginFragmentDirections.actionLoginFragmentToRegisterFragment())
         }
     }
+
+    private fun CharSequence.isValidUsername(): Boolean {
+
+        if (isNullOrEmpty()) {
+
+            loginUsername.requestFocus()
+            loginUsername.error = "نام کاربری خود را وارد نمایید"
+            return false
+
+        } else if (loginUsername.text!!.length > minUsernameLength
+            && !Patterns.EMAIL_ADDRESS.matcher(this).matches()
+        ) {
+
+            return true
+
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(this).matches()) {
+
+            loginUsername.requestFocus()
+            loginUsername.error = "نام کاربری یا ایمیل وارد شده صحیح نیست"
+            return false
+
+        }
+
+        loginUsername.error = null
+        return true
+    }
+
 
     private fun showSystemUI() {
         requireActivity().window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
