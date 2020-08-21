80d625f1e2f9e079bbfd579c3f858c3060fb6242
HosseinMirzaei47 <hosseinhidd@gmail.com>
2020-08-21 12:35:25 +0430

    Fixed issue #9.

--- app/src/main/java/com/example/nattramn/fragments/LoginFragment.kt
+++ app/src/main/java/com/example/nattramn/fragments/LoginFragment.kt
@@ -22,7 +22,7 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        /*showSystemUI()*/
+        showSystemUI()
 
         buttonOnClicks()
 
@@ -43,8 +43,8 @@ class LoginFragment : Fragment() {
 
     private fun showSystemUI() {
         requireActivity().window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+                /*or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN*/)
     }
 
 }
