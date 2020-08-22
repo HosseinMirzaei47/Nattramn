0fcd861c148dad0bbe208e45b35f683d18fc7676
hossein <hosseinhidd@gmail.com>
2020-08-22 11:36:27 +0430

    Removed hard-coded parts of the code

--- app/src/main/java/com/example/nattramn/WriteFragment.kt
+++ app/src/main/java/com/example/nattramn/WriteFragment.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class FragmentWrite : Fragment() {
+class WriteFragment : Fragment() {
 
 
     override fun onCreateView(
