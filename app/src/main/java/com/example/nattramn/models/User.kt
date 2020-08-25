e2f07d2f7fd7e3e2a62b8294ace3939500bca889
hossein <hosseinhidd@gmail.com>
2020-08-25 15:05:30 +0430

    Edited model class User.kt.

--- app/src/main/java/com/example/nattramn/models/User.kt
+++ app/src/main/java/com/example/nattramn/models/User.kt
@@ -4,6 +4,8 @@ data class User(
     val name: String,
     val job: String,
     val image: Int,
+    val followers: Int,
     val favoriteArticles: ArrayList<Article>,
-    val publishedArticle: ArrayList<Article>
+    val publishedArticle: ArrayList<Article>,
+    val isMe: Boolean
 )
