e3d7b5ecd20d9f0a445024ad529f4b9c69eb9d09
HosseinMirzaei47 <hosseinhidd@gmail.com>
2020-08-27 11:58:18 +0430

    Load all RecyclerViews' icon item using the BindingAdapter in User.kt. Changed the default profile picture.

--- app/src/main/java/com/example/nattramn/models/User.kt
+++ app/src/main/java/com/example/nattramn/models/User.kt
@@ -1,5 +1,11 @@
 package com.example.nattramn.models
 
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.nattramn.R
+
 data class User(
     val name: String,
     val job: String,
@@ -9,3 +15,18 @@ data class User(
     val publishedArticle: ArrayList<Article>,
     val isMe: Boolean
 )
+
+@BindingAdapter("imageSource")
+fun setImageUrl(imageView: ImageView, imageSource: String?) {
+
+    imageSource?.let {
+        /*val imgUri = it.toUri().buildUpon().scheme("https://ibb.co/19Z3rbG").build()*/
+        Glide.with(imageView.context)
+            .load(imageSource)
+            .apply(
+                RequestOptions()
+                    .placeholder(R.drawable.default_profile_picture)
+            )
+            .into(imageView)
+    }
+}
