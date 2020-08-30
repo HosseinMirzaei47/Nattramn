0d9bccd7c86107d7ff3cf3187012931e544f45e4
HosseinMirzaei47 <hosseinhidd@gmail.com>
2020-08-30 13:24:55 +0430

    Created Persian Number BindingAdapter in Article.kt.

--- app/src/main/java/com/example/nattramn/models/Article.kt
+++ app/src/main/java/com/example/nattramn/models/Article.kt
@@ -1,5 +1,8 @@
 package com.example.nattramn.models
 
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+
 data class Article(
     val user: User,
     val date: String,
@@ -12,3 +15,28 @@ data class Article(
     val commentsNumber: Int,
     val bookmarked: Boolean
 )
+
+private val faNumbers =
+    arrayOf("۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹")
+
+@BindingAdapter("persianText")
+fun convert(textView: TextView, text: String) {
+
+    var out: String? = ""
+    val length = text.length
+
+    for (i in 0 until length) {
+        val c = text[i]
+        if (c in '0'..'9') {
+            val number = c.toString().toInt()
+            out += faNumbers[number]
+        } else if (c == '٫' || c == ',') {
+            out += '،'
+        } else {
+            out += c
+        }
+    }
+
+    textView.text = out
+
+}
