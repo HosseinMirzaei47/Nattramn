e1051a5579956ea32c9d0ae0b11bf27a0971a005
HosseinMirzaei47 <hosseinhidd@gmail.com>
2020-09-02 11:16:34 +0430

    ArticleFragment initializes its adapters using ArticleRepository.kt and LocalDataSource.kt.

--- app/src/main/java/com/example/nattramn/features/article/ui/viewmodels/ArticleViewModel.kt
+++ app/src/main/java/com/example/nattramn/features/article/ui/viewmodels/ArticleViewModel.kt
@@ -3,22 +3,52 @@ package com.example.nattramn.features.article.ui.viewmodels
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import com.example.nattramn.core.Utils
+import androidx.lifecycle.viewModelScope
+import com.example.nattramn.core.LocalDataSource
+import com.example.nattramn.core.RemoteDataSource
+import com.example.nattramn.features.article.data.ArticleRepository
 import com.example.nattramn.features.article.ui.ArticleView
 import com.example.nattramn.features.article.ui.CommentView
+import kotlinx.coroutines.launch
 import java.util.*
 
 class ArticleViewModel(application: Application) : AndroidViewModel(application) {
 
+    private val articleRepository =
+        ArticleRepository(RemoteDataSource(), LocalDataSource(application))
     var suggestedArticles = MutableLiveData<ArrayList<ArticleView>>()
     var comments = MutableLiveData<ArrayList<CommentView>>()
 
     fun setSuggestedArticles() {
-        suggestedArticles.value = Utils(getApplication())
-            .initArticles()
+        suggestedArticles.value = getArticles()
     }
 
     fun setComments() {
-        comments.value = Utils(getApplication()).initComments()
+        comments.value = getComments()
     }
+
+    private fun getArticles(): ArrayList<ArticleView> {
+
+        var articles = ArrayList<ArticleView>()
+
+        viewModelScope.launch {
+            articles = articleRepository.getArticles()
+        }
+
+        return articles
+
+    }
+
+    private fun getComments(): ArrayList<CommentView> {
+
+        var comments = ArrayList<CommentView>()
+
+        viewModelScope.launch {
+            comments = articleRepository.getComments()
+        }
+
+        return comments
+
+    }
+
 }
---
+++ app/src/main/java/com/example/nattramn/features/user/data/UserRepository.kt
@@ -0,0 +1,4 @@
+package com.example.nattramn.features.user.data
+
+class UserRepository {
+}
