4c7a196a52d1819c0ed4704aa0d7e81475ad29f2
HosseinMirzaei47 <hosseinhidd@gmail.com>
2020-09-09 08:29:57 +0430

    Remote initial commit

--- app/src/main/java/com/example/nattramn/features/user/data/UserRepository.kt
+++ app/src/main/java/com/example/nattramn/features/user/data/UserRepository.kt
@@ -1,4 +1,21 @@
 package com.example.nattramn.features.user.data
 
 class UserRepository {
+
+    companion object {
+        private var myInstance: UserRepository? = null
+        fun getInstance(): UserRepository {
+            if (myInstance == null) {
+                synchronized(this) {
+                    myInstance = UserRepository()
+                }
+            }
+            return myInstance!!
+        }
+    }
+
+    fun loginUser() {
+        
+    }
+
 }
