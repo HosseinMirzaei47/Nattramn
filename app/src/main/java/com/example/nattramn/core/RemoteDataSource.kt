4bf6d225a83b4bb62eedb51b53c4ec527fa0fc75
hosseinmirzaei <hosseinhidd@gmail.com>
2020-09-14 14:52:50 +0430

    Package naming changes

--- app/src/main/java/com/example/nattramn/core/RemoteDataSource.kt
+++ app/src/main/java/com/example/nattramn/core/RemoteDataSource.kt
@@ -1,11 +1,12 @@
 package com.example.nattramn.core
 
 import com.example.nattramn.features.user.data.LoginRequest
+import com.example.nattramn.features.user.data.services.AuthApi
 
 class RemoteDataSource {
 
     suspend fun login(request: LoginRequest) = safeApiCall {
-        ServiceBuilder.buildService(MyApis::class.java).loginUser(request)
+        ServiceBuilder.buildService(AuthApi::class.java).loginUser(request)
     }
 
 }
--- app/src/main/java/com/example/nattramn/features/user/data/services/AuthApi.kt
+++ app/src/main/java/com/example/nattramn/features/user/data/services/AuthApi.kt
@@ -1,18 +1,13 @@
-package com.example.nattramn.core
+package com.example.nattramn.features.user.data.services
 
-import com.example.nattramn.features.article.data.AllArticlesNetwork
 import com.example.nattramn.features.user.data.LoginRequest
 import com.example.nattramn.features.user.data.LoginResponse
 import retrofit2.http.Body
-import retrofit2.http.GET
 import retrofit2.http.POST
 
-interface MyApis {
+interface AuthApi {
 
     @POST("users/login")
     suspend fun loginUser(@Body loginRequest: LoginRequest): LoginResponse
-
-    @GET("articles")
-    suspend fun getArticles(): AllArticlesNetwork
 
 }
